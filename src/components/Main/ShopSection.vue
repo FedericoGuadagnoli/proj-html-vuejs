399bfa9352f94105c2ca3735a5763b03a59d0f63
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-31 22:59:42 +0100

    Create new component ShopSection

---
+++ src/components/Main/ShopSection.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'ShopSection',
+    props: {
+        productsCategories: Array,
+        productsShop: Object
+    }
+}
+</script>
+
+<template>
+
+
+</template>
+
+<style>
+
+</style>
