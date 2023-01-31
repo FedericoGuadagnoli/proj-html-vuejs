adad2d4407d1749b44868891eaa8e6f39d327636
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-31 18:19:58 +0100

    create content for AppButton

--- src/components/Header/AppButton.vue
+++ src/components/Header/AppButton.vue
@@ -5,9 +5,12 @@ export default {
 </script>
 
 <template>
-
+    <button class="btn"></button>
 </template>
 
 <style scoped>
-
+.btn {
+    border-top-right-radius: 5px;
+    border-bottom-left-radius: 5px;
+}
 </style>
