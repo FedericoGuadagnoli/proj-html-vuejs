55e27cdaaa1c79ea44e0a271a07c32bac785834d
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-31 16:44:35 +0100

    Create component AppHeader

---
+++ src/components/Header/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+import HeaderTop from './HeaderTop.vue';
+import { store } from '../../data/store';
+export default {
+    name: 'AppHeader',
+    components: { HeaderTop },
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+
+        <header-top :brands="store.brandsIcon"></header-top>
+
+    </header>
+</template>
+
+<style>
+
+</style>
