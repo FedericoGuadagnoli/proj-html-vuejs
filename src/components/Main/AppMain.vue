7d3dafec9ff75457a0027e8201c104a8f562d37b
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-31 23:00:13 +0100

    import ShopSection in AppMain & prepare props

--- src/components/Main/AppMain.vue
+++ src/components/Main/AppMain.vue
@@ -1,10 +1,11 @@
 <script>
+import ShopSection from './ShopSection.vue';
 import JumbotronSection from './JumbotronSection.vue';
 import NaturalProducts from './NaturalProducts.vue';
 import { store } from '../../data/store';
 export default {
     name: 'AppMain',
-    components: { JumbotronSection, NaturalProducts },
+    components: { JumbotronSection, NaturalProducts, ShopSection },
     data() {
         return {
             store
@@ -17,6 +18,7 @@ export default {
     <main>
         <jumbotron-section></jumbotron-section>
         <natural-products :products="store.naturalProducts"></natural-products>
+        <shop-section :productsCategories="store.categoriesProducts" :productsShop="store.shopProducts"></shop-section>
     </main>
 </template>
 
