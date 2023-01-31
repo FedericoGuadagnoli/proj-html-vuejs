30cd912de71664bffa665d98114ae12786e498db
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-31 16:06:30 +0100

    Create partial content in HeaderTop

--- src/components/Header/HeaderTop.vue
+++ src/components/Header/HeaderTop.vue
@@ -1,11 +1,34 @@
 <script>
 export default {
-    name: 'HeaderTop'
+    name: 'HeaderTop',
+    props: {
+        brand: String
+    }
 }
 </script>
 
 <template>
-
+    <div class="header-top container-fluid">
+        <div class="row">
+            <div class="col-2">
+                <div>
+                    <font-awesome-icon icon="fa-solid fa-location-dot"></font-awesome-icon>
+                    <address>Noth baukhula, tala, USA</address>
+                </div>
+                <div>
+                    <font-awesome-icon icon="fa-solid fa-envelope"></font-awesome-icon>
+                    <span>demo@example.com</span>
+                </div>
+            </div>
+            <div class="col-2">
+                <font-awesome-icon icon="fa-solid fa-clock-four"></font-awesome-icon>
+                <time>8.30AM-8.30PM</time>
+            </div>
+            <div>
+                <font-awesome-icon :icon="brand"></font-awesome-icon>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
