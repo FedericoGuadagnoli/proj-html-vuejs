4257d14bce0246ecf8854cccb72ff9615dd9866d
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-31 23:05:21 +0100

    Create folder Shop Subsection & 3 new components

---
+++ src/components/Main/Shop Subsection/CategoriesSection.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'CategoriesSection'
+}
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+</style>
---
+++ src/components/Main/Shop Subsection/TestimonialSection.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'TestimonialSection'
+}
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+</style>
