ac4bfe3109818fe50c1cc18906c5bf73c155f929
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-31 16:51:07 +0100

    fix styles & classes in HeaderTop

--- src/components/Header/HeaderTop.vue
+++ src/components/Header/HeaderTop.vue
@@ -11,12 +11,12 @@ export default {
     <div class="header-top">
         <div class="container-fluid h-100">
             <div class="row d-flex justify-content-between h-100 text-light">
-                <div class="col-4 d-flex h-100  align-items-center gap-3">
-                    <div class="d-flex gap-1">
+                <div class="col-4 d-flex h-100  align-items-center gap-5">
+                    <div class="d-flex gap-1 align-items-center">
                         <font-awesome-icon class="" icon="fa-solid fa-location-dot"></font-awesome-icon>
                         <address class="m-0">Noth baukhula, tala, USA</address>
                     </div>
-                    <div class="d-flex gap-1">
+                    <div class="d-flex gap-1 align-items-center">
                         <font-awesome-icon icon="fa-solid fa-envelope"></font-awesome-icon>
                         <span>demo@example.com</span>
                     </div>
@@ -41,5 +41,10 @@ export default {
 .header-top {
     background-color: $bg-green;
     height: 3rem;
+
+    .container-fluid {
+        width: 90%;
+        margin: 0 auto;
+    }
 }
 </style>
