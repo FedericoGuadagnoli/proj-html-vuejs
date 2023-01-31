7e6b7e54665b239983a7b9a7aea062f8bdf81fef
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-01 00:51:18 +0100

    import CategoriesSection in ShopSection

--- src/components/Main/ShopSection.vue
+++ src/components/Main/ShopSection.vue
@@ -1,6 +1,8 @@
 <script>
+import CategoriesSection from './Shop Subsection/CategoriesSection.vue';
 export default {
     name: 'ShopSection',
+    components: { CategoriesSection },
     props: {
         productsCategories: Array,
         productsShop: Object
@@ -9,7 +11,10 @@ export default {
 </script>
 
 <template>
-
+    <section id="shop-section">
+        <categories-section :productsCategories="this.productsCategories"
+            :productsShop="this.productsShop"></categories-section>
+    </section>
 
 </template>
 
