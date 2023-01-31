eb56d2a9ccf762afaca1b2f61f6dc0f3c72b96a0
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-31 16:47:21 +0100

    Do content in HeaderTop

--- src/components/Header/HeaderTop.vue
+++ src/components/Header/HeaderTop.vue
@@ -2,35 +2,44 @@
 export default {
     name: 'HeaderTop',
     props: {
-        brand: String
+        brands: String
     }
 }
 </script>
 
 <template>
-    <div class="header-top container-fluid">
-        <div class="row">
-            <div class="col-2">
-                <div>
-                    <font-awesome-icon icon="fa-solid fa-location-dot"></font-awesome-icon>
-                    <address>Noth baukhula, tala, USA</address>
+    <div class="header-top">
+        <div class="container-fluid h-100">
+            <div class="row d-flex justify-content-between h-100 text-light">
+                <div class="col-4 d-flex h-100  align-items-center gap-3">
+                    <div class="d-flex gap-1">
+                        <font-awesome-icon class="" icon="fa-solid fa-location-dot"></font-awesome-icon>
+                        <address class="m-0">Noth baukhula, tala, USA</address>
+                    </div>
+                    <div class="d-flex gap-1">
+                        <font-awesome-icon icon="fa-solid fa-envelope"></font-awesome-icon>
+                        <span>demo@example.com</span>
+                    </div>
                 </div>
-                <div>
-                    <font-awesome-icon icon="fa-solid fa-envelope"></font-awesome-icon>
-                    <span>demo@example.com</span>
+                <div class="col-4 d-flex h-100 justify-content-end align-items-center gap-5">
+                    <div>
+                        <font-awesome-icon icon="fa-solid fa-clock-four"></font-awesome-icon>
+                        <time class="ms-2">8.30AM-8.30PM</time>
+                    </div>
+                    <div>
+                        <font-awesome-icon class="mx-2" v-for="brand in brands" :icon="brand"></font-awesome-icon>
+                    </div>
                 </div>
-            </div>
-            <div class="col-2">
-                <font-awesome-icon icon="fa-solid fa-clock-four"></font-awesome-icon>
-                <time>8.30AM-8.30PM</time>
-            </div>
-            <div>
-                <font-awesome-icon :icon="brand"></font-awesome-icon>
             </div>
         </div>
     </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use '../../assets/scss/partials/variables.scss' as *;
 
+.header-top {
+    background-color: $bg-green;
+    height: 3rem;
+}
 </style>
