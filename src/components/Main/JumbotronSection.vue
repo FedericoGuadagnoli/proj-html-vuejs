87e6666bd66488251eb6fdf254e6a487fa765773
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-31 18:44:11 +0100

    Create new component JumbotronSection

---
+++ src/components/Main/JumbotronSection.vue
@@ -0,0 +1,16 @@
+<script>
+import AppButton from '../AppButton.vue';
+export default {
+    name: 'JumbotronSection',
+    components: { AppButton }
+}
+</script>
+
+<template>
+
+
+</template>
+
+<style>
+
+</style>
