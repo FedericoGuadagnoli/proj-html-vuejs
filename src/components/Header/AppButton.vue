05467cd6e10ae6eed209d349f79cfa69e3e632e8
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-31 18:35:13 +0100

    add solt & style to AppButton

--- src/components/Header/AppButton.vue
+++ src/components/Header/AppButton.vue
@@ -5,12 +5,14 @@ export default {
 </script>
 
 <template>
-    <button class="btn"></button>
+    <button class="btn">
+        <slot></slot>
+    </button>
 </template>
 
 <style scoped>
 .btn {
-    border-top-right-radius: 5px;
-    border-bottom-left-radius: 5px;
+    border-top-right-radius: 20px;
+    border-bottom-left-radius: 20px;
 }
 </style>
