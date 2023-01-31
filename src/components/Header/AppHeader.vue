fe55acd7df7e9808bba1bc3579896b2d4238b9f6
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-31 17:02:39 +0100

    import HeaderBottom in AppHeader

--- src/components/Header/AppHeader.vue
+++ src/components/Header/AppHeader.vue
@@ -1,9 +1,10 @@
 <script>
 import HeaderTop from './HeaderTop.vue';
+import HeaderBottom from './HeaderBottom.vue';
 import { store } from '../../data/store';
 export default {
     name: 'AppHeader',
-    components: { HeaderTop },
+    components: { HeaderTop, HeaderBottom },
     data() {
         return {
             store
@@ -16,7 +17,7 @@ export default {
     <header>
 
         <header-top :brands="store.brandsIcon"></header-top>
-
+        <header-bottom :links="store.Headerlinks"></header-bottom>
     </header>
 </template>
 
