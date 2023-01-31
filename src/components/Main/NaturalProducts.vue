3114f45792fa1879ab9bbbf67f90ca25a9fc9853
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-31 20:09:53 +0100

    Create new component NaturalProducts

---
+++ src/components/Main/NaturalProducts.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'NaturalProducts',
+    props: {
+        products: Array
+    }
+}
+</script>
+
+<template>
+
+
+</template>
+
+<style scoped lang="scss">
+@use '../../assets/scss/partials/variables.scss' as *;
+</style>
