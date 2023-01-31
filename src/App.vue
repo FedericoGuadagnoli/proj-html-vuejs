7c12d6dc6ef4378305ba868bf6480b589bf96b42
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-31 11:42:01 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+@use './assets/scss/style.scss' as *;
+</style>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faClockFour } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faPinterestP } from '@fortawesome/free-brands-svg-icons';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
+library.add(faSearch, faLocationDot, faEnvelope, faClockFour, faFacebookF, faTwitter, faInstagram, faPinterestP, faMagnifyingGlass, faCartShopping, faGooglePlusG);
+import App from './App.vue'
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount('#app');
+
+
