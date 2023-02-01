451108a7d38cb6e9e2f46ab41418168c9c6ab9fc
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-01 12:20:19 +0100

    Add style

--- src/components/Main/Shop Subsection/TestimonialSection.vue
+++ src/components/Main/Shop Subsection/TestimonialSection.vue
@@ -7,23 +7,30 @@ export default {
 <template>
     <subsection id="testimonial">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <figure>
-                        <img src="../../../assets/img/testi-icon.png" alt="icon">
-                    </figure>
-                    <p>"Dessert pudding dessert jelly beans cupkace sweet caramels gingerbread. Fruitcake niscuit
-                        cheescake. Cookie topping sweets muffine pudding tart bear claw sugar plum croissant". We
-                        started as a small legal consultancy. We have proved our competence and had many satisfied
-                        clients.</p>
-                    <h4 class="text-capitalize">martha alex</h4>
-                    <span class="role text-capitalize">manager</span>
+            <div class="row mt-5 pt-5">
+                <div class="col-12 d-flex justify-content-center">
+                    <div class="card w-50 text-center border-0">
+                        <figure class="h-25 d-flex align-items-center justify-content-center">
+                            <img src="../../../assets/img/testi-icon.png" alt="icon" class="img-fluid h-50">
+                        </figure>
+                        <p class="fw-semibold">"Dessert pudding dessert jelly beans cupkace sweet caramels gingerbread.
+                            Fruitcake niscuit
+                            cheescake. Cookie topping sweets muffine pudding tart bear claw sugar plum croissant". We
+                            started as a small legal consultancy. We have proved our competence and had many satisfied
+                            clients.</p>
+                        <h4 class="text-capitalize fw-bold">martha alex</h4>
+                        <span class="role text-capitalize fw-bold">manager</span>
+                    </div>
                 </div>
             </div>
         </div>
     </subsection>
 </template>
 
-<style>
+<style scoped lang="scss">
+@use '../../../assets/scss/partials/variables.scss' as *;
 
+.role {
+    color: $color-orange;
+}
 </style>
