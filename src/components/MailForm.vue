d192c5c3fe46719d3db6f44e6b1984618120c1c9
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-01 19:12:49 +0100

    Create new component MailForm

---
+++ src/components/MailForm.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'MailForm'
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+@use '../assets/scss/partials/variables.scss' as *;
+</style>
