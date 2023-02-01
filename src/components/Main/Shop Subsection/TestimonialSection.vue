dbae7da2745d59c6176f15cff6675df5fec9f337
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-01 11:44:42 +0100

    Create content in TestimonialSection

--- src/components/Main/Shop Subsection/TestimonialSection.vue
+++ src/components/Main/Shop Subsection/TestimonialSection.vue
@@ -5,7 +5,23 @@ export default {
 </script>
 
 <template>
-
+    <subsection id="testimonial">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <figure>
+                        <img src="../../../assets/img/testi-icon.png" alt="icon">
+                    </figure>
+                    <p>"Dessert pudding dessert jelly beans cupkace sweet caramels gingerbread. Fruitcake niscuit
+                        cheescake. Cookie topping sweets muffine pudding tart bear claw sugar plum croissant". We
+                        started as a small legal consultancy. We have proved our competence and had many satisfied
+                        clients.</p>
+                    <h4 class="text-capitalize">martha alex</h4>
+                    <span class="role text-capitalize">manager</span>
+                </div>
+            </div>
+        </div>
+    </subsection>
 </template>
 
 <style>
