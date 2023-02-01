a61fb36464c7752df61fa66920e663067d66dc4a
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-01 11:46:19 +0100

    import TestimonialSection in ShopSection

--- src/components/Main/ShopSection.vue
+++ src/components/Main/ShopSection.vue
@@ -1,8 +1,9 @@
 <script>
+import TestimonialSection from './Shop Subsection/TestimonialSection.vue';
 import CategoriesSection from './Shop Subsection/CategoriesSection.vue';
 export default {
     name: 'ShopSection',
-    components: { CategoriesSection },
+    components: { CategoriesSection, TestimonialSection },
     props: {
         productsCategories: Array,
         productsShop: Object
@@ -14,6 +15,7 @@ export default {
     <section id="shop-section">
         <categories-section :productsCategories="this.productsCategories"
             :productsShop="this.productsShop"></categories-section>
+        <testimonial-section></testimonial-section>
     </section>
 
 </template>
