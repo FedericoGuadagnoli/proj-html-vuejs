68c91e4af0583adc1a69f7249d05b7eb2823fcf0
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-01 16:26:46 +0100

    Create content in DiscountSection

--- src/components/Main/Shop Subsection/DiscountSection.vue
+++ src/components/Main/Shop Subsection/DiscountSection.vue
@@ -1,13 +1,52 @@
 <script>
 export default {
-    name: 'DiscountSection'
+    name: 'DiscountSection',
+    props: {
+        discounts: Object
+    },
+    methods: {
+        buildImagePath(image) {
+            const url = new URL(`../../../assets/img/${image}`,
+                import.meta.url);
+            return url.href;
+        }
+    }
 }
 </script>
 
 <template>
-
+    <subsection id="maxi-discount">
+        <div class="container-fluid">
+            <div class="row d-flex justify-content-center py-5 my-5">
+                <div v-for="scount in discounts" class="col-auto">
+                    <figure>
+                        <img :src="buildImagePath(scount.image)" alt="tomato" class="img-fluid">
+                    </figure>
+                    <figcaption class="d-flex flex-column justify-content-between">
+                        <h2 class="text-light fw-bolder">{{ scount.text }}</h2>
+                        <a class="text-light fs-5 text-uppercase fw-bold" href="#">{{ scount.link }}</a>
+                    </figcaption>
+                </div>
+            </div>
+        </div>
+    </subsection>
 </template>
 
-<style>
+<style scoped lang="scss">
+.col-auto {
+    position: relative;
 
+    figcaption {
+        position: absolute;
+        top: 5%;
+        bottom: 15%;
+        left: 5%;
+        width: 70%;
+
+        a {
+            text-decoration: none;
+            letter-spacing: -1px;
+        }
+    }
+}
 </style>
