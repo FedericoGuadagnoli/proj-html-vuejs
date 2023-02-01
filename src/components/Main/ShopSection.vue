d963f2a940bef579f742764c56655ca8b79d0e27
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-01 15:48:51 +0100

    Import TrendingFood in ShopSection & prepare props

--- src/components/Main/ShopSection.vue
+++ src/components/Main/ShopSection.vue
@@ -1,10 +1,11 @@
 <script>
+import TrendingFood from './Shop Subsection/TrendingFood.vue';
 import DiscountSection from './Shop Subsection/DiscountSection.vue';
 import TestimonialSection from './Shop Subsection/TestimonialSection.vue';
 import CategoriesSection from './Shop Subsection/CategoriesSection.vue';
 export default {
     name: 'ShopSection',
-    components: { CategoriesSection, TestimonialSection, DiscountSection },
+    components: { CategoriesSection, TestimonialSection, DiscountSection, TrendingFood },
     props: {
         productsCategories: Array,
         productsShop: Object,
@@ -19,6 +20,7 @@ export default {
             :productsShop="this.productsShop"></categories-section>
         <testimonial-section></testimonial-section>
         <discount-section :strengths="this.strengths"></discount-section>
+        <trending-food :productsShop="this.productsShop"></trending-food>
     </section>
 
 </template>
