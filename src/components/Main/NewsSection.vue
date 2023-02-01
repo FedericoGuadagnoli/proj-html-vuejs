f29cef4403f557b2bb5737bf40a70eac78f6c5c4
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-01 19:05:33 +0100

    create content in NewsSection

--- src/components/Main/NewsSection.vue
+++ src/components/Main/NewsSection.vue
@@ -1,13 +1,100 @@
 <script>
 export default {
-    name: 'NewsSection'
+    name: 'NewsSection',
+    props: {
+        news: Object,
+        logos: Array
+    },
+    methods: {
+        buildImagePath(image) {
+            const url = new URL(`../../assets/img/${image}`,
+                import.meta.url);
+            return url.href;
+        }
+    }
 }
 </script>
 
 <template>
-
+    <section id="news">
+        <div class="container-fluid">
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="col-12 text-center">
+                    <h4 class="mt-5 fw-bold">The news what we have</h4>
+                    <h1 class="text-uppercase fw-bolder">gogrin latest
+                        <span>news</span>
+                        feed
+                    </h1>
+                </div>
+                <div v-for="news in this.news" class="col-2 my-5 d-flex flex-column gap-3">
+                    <figure>
+                        <img :src="buildImagePath(news.img)" alt="food" class=" img-fluid">
+                    </figure>
+                    <figcaption class="d-flex gap-3">
+                        <cite class="text-capitalize">
+                            <font-awesome-icon class="icon" icon="fa-solid fa-user" />
+                            {{ news.name }}
+                        </cite>
+                        <time>
+                            <font-awesome-icon class="icon" icon="fa-solid fa-clock-four"></font-awesome-icon>
+                            {{ news.date }}
+                        </time>
+                    </figcaption>
+                    <h5 class="fw-bolder">{{ news.title }}</h5>
+                    <p>{{ news.text }}</p>
+                    <a class="text-uppercase fw-bold" href="#">{{ news.link }}</a>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center gap-5 pb-5 my-5">
+                <div v-for="logo in this.logos" class="col-1">
+                    <figure>
+                        <img :src="buildImagePath(logo)" alt="logo">
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
-<style>
+<style scoped lang="scss">
+@use '../../assets/scss/partials/variables.scss' as *;
 
+.container-fluid {
+    margin-bottom: 10rem;
+}
+
+.col-12 {
+    background-image: url(../../assets/img/title-shap.png);
+    background-repeat: no-repeat;
+    background-position: center;
+
+    h4,
+    h1 span {
+        color: $color-orange;
+    }
+
+    h1 {
+        font-size: 3rem;
+    }
+}
+
+figcaption {
+
+    cite,
+    time {
+        .icon {
+            color: $color-orange;
+        }
+
+    }
+}
+
+.col-2 {
+
+    h5,
+    a {
+        text-decoration: none;
+        color: $color-green;
+    }
+}
 </style>
