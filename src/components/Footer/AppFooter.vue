199c1c8a0c4ccdc6f884e97ba9bb61c0afdbe6c1
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-02 02:47:06 +0100

    import footerBottom in AppFooter & preparo drops

--- src/components/Footer/AppFooter.vue
+++ src/components/Footer/AppFooter.vue
@@ -1,9 +1,10 @@
 <script>
 import { store } from '../../data/store';
+import FooterBottom from './FooterBottom.vue';
 import FooterTop from './FooterTop.vue';
 export default {
     name: 'AppFooter',
-    components: { FooterTop },
+    components: { FooterTop, FooterBottom },
     data() {
         return {
             store
@@ -16,6 +17,7 @@ export default {
     <footer>
         <footer-top :icons="store.brandsIcon" :links="store.Footerlinks" :blogs="store.blogs"
             :gallery="store.gallery"></footer-top>
+        <footer-bottom :info="store.info" :links="store.links"></footer-bottom>
     </footer>
 </template>
 
