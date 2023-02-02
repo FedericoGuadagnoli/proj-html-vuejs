5709421ab1c1bdd24bcb281520fdb5cf0d65b0c8
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-02 01:30:38 +0100

    Import MailForm in AppMain and do style

--- src/components/Main/AppMain.vue
+++ src/components/Main/AppMain.vue
@@ -1,4 +1,5 @@
 <script>
+import MailForm from '../MailForm.vue';
 import NewsSection from './NewsSection.vue';
 import ShopSection from './ShopSection.vue';
 import JumbotronSection from './JumbotronSection.vue';
@@ -6,7 +7,7 @@ import NaturalProducts from './NaturalProducts.vue';
 import { store } from '../../data/store';
 export default {
     name: 'AppMain',
-    components: { JumbotronSection, NaturalProducts, ShopSection, NewsSection },
+    components: { JumbotronSection, NaturalProducts, ShopSection, NewsSection, MailForm },
     data() {
         return {
             store
@@ -22,9 +23,19 @@ export default {
         <shop-section :productsCategories="store.categoriesProducts" :productsShop="store.shopProducts"
             :strengths="store.strengths" :discounts="store.discounts"></shop-section>
         <news-section :news="store.news" :logos="store.logos"></news-section>
+        <mail-form class="mail-form"></mail-form>
     </main>
 </template>
 
-<style>
+<style scoped lang="scss">
+main {
+    position: relative;
+    margin-bottom: 5rem;
 
+    .mail-form {
+        position: absolute;
+        width: 100%;
+        bottom: -3.5%;
+    }
+}
 </style>
